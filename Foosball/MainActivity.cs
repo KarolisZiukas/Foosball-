d35c4a55b0a99fc377dd2bc868d08b36684181fe
Karolis <karolis.ziu@gmail.com>
2017-09-11 11:16:56 +0300

    Initial commit

---
+++ Foosball/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Foosball
+{
+    [Activity(Label = "Foosball", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
